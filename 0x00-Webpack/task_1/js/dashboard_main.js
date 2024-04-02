191be79a4a0c4d60fab5627cc9e3c0ceec33a3a2
254guru <otienooluda@gmail.com>
2024-04-02 20:49:44 +0000

    update config files

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -14,6 +14,6 @@ $('#clickButton').on('click', _.debounce(updateCounter, 1000));
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button id="clickButton">Click here to get started</button>');
-$('body').append("<p id="count"></p>");
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
