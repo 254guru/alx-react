3155869532b343911b91c5c3698bc36e2a9de4c6
254guru <otienooluda@gmail.com>
2024-04-08 17:51:02 +0000

    Basic components, alx-react props

---
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,31 @@
+import { getFullYear, getFooterCopy } from './utils';
+import logo from './holberton-logo.jpg';
+import './App.css';
+import './Notifications';
+import './Login';
+import './Footer';
+import './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="APP-body">
+	<p>Login to access the full dashboard</p>
+	  <label htmlFor="email">email</label>
+	  <input name="email" type="email" id="email"></input>
+	  <label htmlFor="password">password</label>
+	  <input name="password" type="password" id="passsword"></input>
+	  <button>OK</button>
+      </div>
+      <div className="App-footer">
+	  <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
