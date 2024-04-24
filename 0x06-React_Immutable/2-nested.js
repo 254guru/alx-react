ac79f6eb5aab8bf6b197a5924c80ad42a2055487
254guru <otienooluda@gmail.com>
2024-04-24 14:13:43 +0000

    update script

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,15 +1,7 @@
-import { fromJS, Map } from 'immutable';
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-    let result = fromJS(object);
-    for (const key of array) {
-        if (Map.isMap(result)) {
-            result = result.get(key);
-        } else if (typeof result === 'object' && result !== null) {
-            result = result[key];
-        } else {
-            return undefined;
-        }
-    }
-    return result;
+  const mappedObj = fromJS(object);
+
+  return mappedObj.getIn(array, undefined);
 }
