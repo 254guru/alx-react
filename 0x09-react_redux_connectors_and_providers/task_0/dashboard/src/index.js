261daa9093f0970f3e362a9dbcd744cb9f6dc811
254guru <otienooluda@gmail.com>
2024-05-10 17:58:02 +0300

    small store

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,3 +1,16 @@
-const element = document.createElement('div');
-element.innerHTML = 'Hello, Webpack!';
-document.getElementById('app').appendChild(element);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux'; // Import createStore from Redux
+import { Provider } from 'react-redux'; // Import Provider from react-redux
+import App from './App/App';
+import uiReducer from './reducers/uiReducer'; // Import uiReducer
+
+const store = createStore(uiReducer); // Create a Redux store containing uiReducer state
+
+ReactDOM.render(
+  <Provider store={store}> {/* Pass the store to Provider */}
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
+
